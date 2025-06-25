10b7628c66f8c7b75774f9e0caa8e7941cea5402
Ratnadip12 <146317851+Ratnadip12@users.noreply.github.com>
2025-06-26 00:19:32 +0530

    mobile.js

--- mobile.js
+++ mobile.js
@@ -5,58 +5,60 @@ class Paper {
   constructor(paper) {
     this.paper = paper;
     this.holdingPaper = false;
-    this.touchX = 0;
-    this.touchY = 0;
-    this.prevTouchX = 0;
-    this.prevTouchY = 0;
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.prevMouseX = 0;
+    this.prevMouseY = 0;
+    this.velX = 0;
+    this.velY = 0;
     this.currentPaperX = 0;
     this.currentPaperY = 0;
     this.init();
   }
 
   init() {
-    this.paper.addEventListener('touchstart', (e) => {
+    document.addEventListener('mousemove', (e) => {
+      this.mouseX = e.clientX;
+      this.mouseY = e.clientY;
+
+      this.velX = this.mouseX - this.prevMouseX;
+      this.velY = this.mouseY - this.prevMouseY;
+
+      if (this.holdingPaper) {
+        this.currentPaperX += this.velX;
+        this.currentPaperY += this.velY;
+
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+
+        this.paper.style.transform = `translate(${this.currentPaperX}px, ${this.currentPaperY}px)`;
+      }
+    });
+
+    this.paper.addEventListener('mousedown', (e) => {
+      if (this.holdingPaper) return;
       this.holdingPaper = true;
 
-      const touch = e.touches[0];
-      this.touchX = this.prevTouchX = touch.clientX;
-      this.touchY = this.prevTouchY = touch.clientY;
-
       this.paper.style.zIndex = highestZ++;
+      this.prevMouseX = this.mouseX;
+      this.prevMouseY = this.mouseY;
 
-      if (this.paper.classList.contains('heart') && !musicStarted) {
-        const music = document.getElementById('bg-music');
-        music.play();
+      // Play background music if it's the heart paper
+      if (this.paper.classList.contains('paper') && !musicStarted) {
+        const bgMusic = document.getElementById('bg-music');
+        bgMusic.play();
         musicStarted = true;
       }
     });
 
-    this.paper.addEventListener('touchmove', (e) => {
-      if (!this.holdingPaper) return;
-
-      const touch = e.touches[0];
-      this.touchX = touch.clientX;
-      this.touchY = touch.clientY;
-
-      const deltaX = this.touchX - this.prevTouchX;
-      const deltaY = this.touchY - this.prevTouchY;
-
-      this.currentPaperX += deltaX;
-      this.currentPaperY += deltaY;
-
-      this.paper.style.transform = `translate(${this.currentPaperX}px, ${this.currentPaperY}px)`;
-
-      this.prevTouchX = this.touchX;
-      this.prevTouchY = this.touchY;
-
-      e.preventDefault();
-    });
-
-    this.paper.addEventListener('touchend', () => {
+    window.addEventListener('mouseup', () => {
       this.holdingPaper = false;
     });
   }
 }
 
+ function toggleFold(element) {
+    element.classList.toggle('open');
+  }
 const papers = Array.from(document.querySelectorAll('.paper'));
 papers.forEach(paper => new Paper(paper));
